f0f91d6c655a3323b4e7aa459fc5d1f0a1809a3d
Sukky147 <jessieyan98@gmail.com>
2021-11-09 14:04:34 -0500

    Revert "AR"
    This reverts commit 9b1c106def73bc738729291e25ea4e52f9bf79f0.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import ssl
  
 ## Variables you can modify
  
-bind_to_address = '192.168.250.144'
+bind_to_address = '192.168.1.208'
 server_port = 8000
 ssl_key_file = "server1.example.com.key"
 ssl_certificate_file = "server1.example.com.pem"
